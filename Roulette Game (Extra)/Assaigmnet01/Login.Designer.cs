fcca8e99ed6c9bf350fd9d130e06b667c61ace28
Yanek2000 <Admin@zefir.cu.ma>
2019-12-12 23:47:25 +0000

    Added Login screen

---
+++ Roulette Game (Extra)/Assaigmnet01/Login.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Assaigmnet01
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameImput = new System.Windows.Forms.TextBox();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UsernameImput
+            // 
+            this.UsernameImput.Location = new System.Drawing.Point(32, 53);
+            this.UsernameImput.Name = "UsernameImput";
+            this.UsernameImput.Size = new System.Drawing.Size(214, 20);
+            this.UsernameImput.TabIndex = 0;
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Location = new System.Drawing.Point(32, 93);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.Size = new System.Drawing.Size(214, 20);
+            this.PasswordInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(81, 147);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(129, 38);
+            this.LoginBtn.TabIndex = 4;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Assaigmnet01.Properties.Resources.LoginBack;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(276, 197);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.UsernameImput);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Login_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Login_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UsernameImput;
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
---
+++ Roulette Game (Extra)/Assaigmnet01/Login.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Assaigmnet01
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        //fullscreen fix
+        Point lastPoint;
+        private void Login_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+        private void Login_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+        //fulscreen fix
+
+        int Counter = 3;
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if (Counter == 1)
+            {
+                MessageBox.Show("Attempts Exceeded program will now close","Login Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                Thread.Sleep(3000);
+                Application.Exit();
+            }
+            else
+            {
+                string Username = UsernameImput.Text;
+                string Password = PasswordInput.Text;
+                if (Password.Equals("admin") && Username.Equals("admin"))
+                {
+                    Game myForm = new Game();
+                    this.Hide();
+                    myForm.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    Counter = Counter - 1;
+                    MessageBox.Show("Login error you have " + Counter.ToString() + " Attempts left", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                }
+            }
+        }
+
+
+    }
+}
